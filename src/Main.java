1077a7446d4d0dcb5d0984e524fe9265e12021f1
lucas <lucasperuchena.27@gmail.com>
2023-05-24 20:10:55 +0200

    creación de menú

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mi primer cambio 2");
+        System.out.println("este es el menu para mostrar:");
+        System.out.println("1.asdasdas");
+        System.out.println("2.blablabla");
     }
 }
