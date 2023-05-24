fdedc0f3d2ea1120e0f3c53e559c9a281c5e87c3
lucas <lucasperuchena.27@gmail.com>
2023-05-24 20:39:18 +0200

    nueva opción añadida al menú (opción 4)

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("1.asdasdas");
         System.out.println("2.blablabla");
         System.out.println("3. nueva opción");
+        System.out.println("4. asdasd");
     }
 }
